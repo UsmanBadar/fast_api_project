c7d5f497d4b0e4e862d5c7c8c156718446a2c2ba
Usman <usman.badar006@gmail.com>
2025-12-10 11:01:58 +1100

    pydantic schemas

---
+++ app/schemas/user_model.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: str | None = None
+    is_active: bool = True
+    is_superuser: bool = False
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    full_name: str | None = None
+
+
+class UserRead(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class UserInDB(UserBase):
+    id: int
+    hashed_password: str
