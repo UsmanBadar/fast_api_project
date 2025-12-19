1a7801e3bf001293eddc16fd1f55fcd8e02e2baa
Usman <usman.badar006@gmail.com>
2025-12-19 13:17:05 +1100

    moved all config to settings

--- app/core/config.py
+++ app/core/config.py
@@ -3,20 +3,25 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-ENV_PATH = BASE_DIR / ".env_1"
+ENV_PATH = BASE_DIR / ".env"
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Realtime Analytics API"
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int 
+    REFRESH_TOKEN_EXPIRE_DAYS: int 
+    ALGORITHM: str
     SECRET_KEY: str
     REFRESH_SECRET_KEY: str
+    DB_PASSWORD: str
+    CLOUD_SERVER_ADMIN: str
+    SERVER: str
+    DATABASE: str
 
     model_config = SettingsConfigDict(
         env_file=str(ENV_PATH),
         env_file_encoding="utf-8",
     )
 
+settings = Settings()
